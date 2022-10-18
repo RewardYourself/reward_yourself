8a75e1708ac418ccd9fc8159575b7b8be4866cda
Paulo Vitor Fernandes Andrade <paulovitorfandrade2002@gmail.com>
2022-10-17 23:30:13 -0300

    Fixed Bugs

--- lib/app/modules/authentication/presenter/forgetPassword_page.dart
+++ lib/app/modules/authentication/presenter/forgetPassword_page.dart
@@ -1,6 +1,3 @@
-import 'dart:html';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_modular/flutter_modular.dart';
